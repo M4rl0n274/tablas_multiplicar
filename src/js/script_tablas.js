0109bc82d92daf1421d3dd24cabde08e040c6661
M4rl0n274 <marlonquinteri274@gmail.com>
2025-03-06 18:19:02 -0500

    control sobre maxima cantidad 0

--- src/js/script_tablas.js
+++ src/js/script_tablas.js
@@ -1,21 +1,26 @@
+function enviarTablas() {
+  const tabla = document.getElementById("tabla").value;
+  const hasta = document.getElementById("hasta").value;
+  const resultadoP = document.getElementById("resultado");
+  const advertenciaP = document.getElementById("advertencia");
 
-function enviarTablas(){
-    
-    const tabla = document.getElementById('tabla').value;
-    const resultadoDiv = document.getElementById('Resultado');
-    const hasta = document.getElementById('hasta').value;
+  if (hasta == null || hasta == 0) {
+    let mensaje =
+      "Recuerda que puedes especificar hasta que numero quieres la tabla";
+    advertenciaP.innerHTML = mensaje;
+  }
 
-    let resultadoHTML  ="<h2>Resultado</h2><br>"+ "La tabla del " +tabla + " Es:" + "<br>"
+  let resultadoHTML =
+    "<h2>Resultado</h2><br>" + "La tabla del " + tabla + " Es:" + "<br>";
 
-    if(hasta == 0){
-        for (let i = 0 ; i<=10 ; i++){
-                resultadoHTML  += tabla +" X " + i + " = " + tabla * i +"<br>";
-        }
-    }else{ 
-        for (let i = 0 ; i<=hasta ; i++){
-                resultadoHTML  += tabla +" X " + i + " = " + tabla * i +"<br>";
-        }
+  if (hasta == 0 || hasta >=1001) {
+    for (let i = 0; i <= 10; i++) {
+      resultadoHTML += tabla + " X " + i + " = " + tabla * i + "<br>";
     }
-    resultadoDiv.innerHTML =  resultadoHTML;
-
+  } else {
+    for (let i = 0; i <= hasta; i++) {
+      resultadoHTML += tabla + " X " + i + " = " + tabla * i + "<br>";
+    }
+  }
+  resultadoP.innerHTML = resultadoHTML;
 }
