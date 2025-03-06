4b162beabf310725a642670bf93f7f37c9062954
M4rl0n274 <marlonquinteri274@gmail.com>
2025-03-06 17:59:01 -0500

    logica mejorada

--- src/js/script_tablas.js
+++ src/js/script_tablas.js
@@ -1,31 +1,21 @@
-function enviarTablas(){
 
+function enviarTablas(){
+    
     const tabla = document.getElementById('tabla').value;
     const resultadoDiv = document.getElementById('Resultado');
+    const hasta = document.getElementById('hasta').value;
 
+    let resultadoHTML  ="<h2>Resultado</h2><br>"+ "La tabla del " +tabla + " Es:" + "<br>"
 
-    let resultadoHTML  = "La tabla del " +tabla + " Es:" + "<br>"
-
-    for (let i = 0 ; i<=10 ; i++){
-        resultadoHTML  += tabla +" X " + i + " = " + tabla * i +"<br>";
+    if(hasta == 0){
+        for (let i = 0 ; i<=10 ; i++){
+                resultadoHTML  += tabla +" X " + i + " = " + tabla * i +"<br>";
+        }
+    }else{ 
+        for (let i = 0 ; i<=hasta ; i++){
+                resultadoHTML  += tabla +" X " + i + " = " + tabla * i +"<br>";
+        }
     }
-    
     resultadoDiv.innerHTML =  resultadoHTML;
- 
-
-    
-
-
 
 }
-
-
-
-
-
-
-
-
-
-
-
