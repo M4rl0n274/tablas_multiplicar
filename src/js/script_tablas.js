0222029c212624df998d56ab62906e66838a9e3f
M4rl0n274 <marlonquinteri274@gmail.com>
2025-03-05 23:23:20 -0500

    logica de las tablas implementada

--- src/js/script_tablas.js
+++ src/js/script_tablas.js
@@ -1,11 +1,28 @@
+function enviarTablas(){
+
+    const tabla = document.getElementById('tabla').value;
+    const resultadoDiv = document.getElementById('Resultado');
 
 
+    let resultadoHTML  = "La tabla del " +tabla + " Es:" + "<br>"
 
-
-for (let i = 0 ; i<=10 ; i++){
-    console.log(i);
+    for (let i = 0 ; i<=10 ; i++){
+        resultadoHTML  += tabla +" X " + i + " = " + tabla * i +"<br>";
+    }
     
+    resultadoDiv.innerHTML =  resultadoHTML;
+ 
+
+    
+
+
+
 }
+
+
+
+
+
 
 
 
